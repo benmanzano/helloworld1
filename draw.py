57f71c342e25035b4eb7e27f820c8bb6fe8a1b34
BenM <131933733+benmanzano@users.noreply.github.com>
2025-04-09 16:25:49 -0500

    upload new

---
+++ draw.py
@@ -0,0 +1,14 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+painter.pensize(20)
+
+for _ in range (18):
+    painter.forward(100)
+    painter.left(100)
+
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 85708a6 (repeat 4 times with a for loop)
+turtle.done()
